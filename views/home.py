cfbcd16f145efe196a3bd7ebccbdc20b265658b9
mettlsi1 <mettlsi1@students.zhaw.ch>
2026-03-04 08:59:47 +0100

    Email update

--- views/home.py
+++ views/home.py
@@ -1,13 +1,12 @@
 import streamlit as st
 
 st.title("Halbwertszeit-Rechner")
-st.subheader("Die Anwendung ermöglicht es ihnen die Halbwertszeit eines Elements zu berechnen")
+st.markdown("Die Anwendung ermöglicht es ihnen die Halbwertszeit eines Elements zu berechnen")
 
 # Disclaimer mit gelbem Hintergrund
 st.warning("⚠️ Hinweis: Dies ist eine Beispielanwendung. Bitte überprüfen Sie die Ergebnisse mit offiziellen Quellen.")
 
 
 """
-Diese App wurde von der folgenden Person entwickelt:
-- Simon Mettler (mettlsi1@students.zhaw.ch)
+Diese App wurde von Simon Mettler (mettlsi1@students.zhaw.ch) im Rahmen des Moduls 'BMLD Informatik 2' an der ZHAWentwickelt.
 """
