accf734ad8c138ee1207004b95b00605153c338d
mettlsi1 <mettlsi1@students.zhaw.ch>
2026-03-04 09:33:47 +0100

    Bug-Fix

--- views/Unterseite_A.py
+++ views/Unterseite_A.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Unterseite A")
+st.title("Halbwertszeit-Rechner")
 
 # Eingabefeld für Halbwertszeit
 halbwertszeit = st.number_input("Halbwertszeit eingeben (in Jahren):", min_value=0.0, step=0.1)
