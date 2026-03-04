be9c3fd14b9efff9aecfc3a137231bf181fe8430
mettlsi1 <mettlsi1@students.zhaw.ch>
2026-03-04 09:15:05 +0100

    Eingabefeld für Halbwertszeit

--- views/Unterseite_A.py
+++ views/Unterseite_A.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.title("Halbertszeit-Rechner")
+
+# Eingabefeld für Halbwertszeit
+halbwertszeit = st.number_input("Halbwertszeit eingeben (in Jahren):", min_value=0.0, step=0.1)
+
+st.write(f"Eingegebene Halbwertszeit: {halbwertszeit}")
