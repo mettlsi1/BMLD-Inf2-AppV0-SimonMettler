556619a40aa9b7e031aec04f751511e60f6ba06c
mettlsi1 <mettlsi1@students.zhaw.ch>
2026-03-17 23:12:39 +0100

    session state update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 if 'data_df' not in st.session_state:
     st.session_state['data_df'] = pd.DataFrame()
 
-st.set_page_config(page_title="Meine App", page_icon=":material/home:")
+st.set_page_config(page_title="Halbwertszeitrechner", page_icon=":material/home:")
 
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
 pg_second = st.Page("views/halbwertszeitrechner.py", title="Halbwertszeit-Rechner", icon="☢️")
