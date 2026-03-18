aef3ab3d29bc31ec9b51177ed5c0ec9db0ef01ae
mettlsi1 <mettlsi1@students.zhaw.ch>
2026-03-18 12:31:00 +0100

    codes verschlankt

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 from utils.data_manager import DataManager
 
-# --- NEW CODE: import and initialize data manager and login manager ---
+# --- importieren und initialisieren des DataManagers ---
 from utils.data_manager import DataManager
 from utils.login_manager import LoginManager
 
@@ -12,16 +12,15 @@ data_manager = DataManager(       # initialize data manager
     ) 
 login_manager = LoginManager(data_manager) # handles user login and registration
 login_manager.login_register()             # stops if not logged in
-# --- END OF NEW CODE ---
 
-# --- CODE UPDATE: load user data from data manager if not already present in session state --
+
+# --- user data laden, falls noch nicht vorhanden neue erstellen --
 if 'data_df' not in st.session_state:
     st.session_state['data_df'] = data_manager.load_user_data(
         'data.csv',                     # The file on switch drive where the data is stored
         initial_value=pd.DataFrame(),   # Initial value if the file does not exist
         parse_dates=['timestamp']       # Parse timestamp as datetime
     )
-# --- END OF CODE UPDATE ---
 
 st.set_page_config(page_title="Halbwertszeitrechner", page_icon=":material/home:")
 
--- views/halbwertszeitrechner.py
+++ views/halbwertszeitrechner.py
@@ -78,10 +78,10 @@ if submit:
             [st.session_state['data_df'], pd.DataFrame([result])],
         )
 
- # --- CODE UPDATE: save data to data manager ---
+ # --- Daten auf DataManager speichern ---
         data_manager = DataManager()
         data_manager.save_user_data(st.session_state['data_df'], 'data.csv')
-    # --- END OF CODE UPDATE ---
+
        
 st.markdown("### Verlauf / Session-Historie")
 st.dataframe(st.session_state['data_df'])
