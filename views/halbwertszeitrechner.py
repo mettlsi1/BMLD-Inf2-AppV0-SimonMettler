2f1bfb8545559b68a6b3976aab518b8178aa2899
mettlsi1 <mettlsi1@students.zhaw.ch>
2026-03-18 12:44:53 +0100

    bug fix

--- views/halbwertszeitrechner.py
+++ views/halbwertszeitrechner.py
@@ -81,7 +81,11 @@ if submit:
  # --- Daten auf DataManager speichern ---
         data_manager = DataManager()
         data_manager.save_user_data(st.session_state['data_df'], 'data.csv')
-
+        
+st.session_state['data_df'] = pd.concat(
+    [st.session_state['data_df'], pd.DataFrame([result])],
+    ignore_index=True
+)
        
 st.markdown("### Verlauf / Session-Historie")
 st.dataframe(st.session_state['data_df'])
