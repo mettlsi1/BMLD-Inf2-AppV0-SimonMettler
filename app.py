0af586f698ea80eccd37119488147168f4981ca1
mettlsi1 <mettlsi1@students.zhaw.ch>
2026-03-18 11:07:36 +0100

    DataManager von Samuel in Übung kopiert

--- app.py
+++ app.py
@@ -2,11 +2,26 @@ import pandas as pd
 import streamlit as st
 from utils.data_manager import DataManager
 
-data_manager = DataManager(fs_protocol='webdav', fs_root_folder="BMLD_App_HWZR")
+# --- NEW CODE: import and initialize data manager and login manager ---
+from utils.data_manager import DataManager
+from utils.login_manager import LoginManager
 
-# --- initialize empty data frame if not already present ---
+data_manager = DataManager(       # initialize data manager
+    fs_protocol='webdav',         # protocol for the filesystem, use webdav for switch drive
+    fs_root_folder="BMLD_App_HWZR"  # folder on switch drive where the data is stored
+    ) 
+login_manager = LoginManager(data_manager) # handles user login and registration
+login_manager.login_register()             # stops if not logged in
+# --- END OF NEW CODE ---
+
+# --- CODE UPDATE: load user data from data manager if not already present in session state --
 if 'data_df' not in st.session_state:
-    st.session_state['data_df'] = pd.DataFrame()
+    st.session_state['data_df'] = data_manager.load_user_data(
+        'data.csv',                     # The file on switch drive where the data is stored
+        initial_value=pd.DataFrame(),   # Initial value if the file does not exist
+        parse_dates=['timestamp']       # Parse timestamp as datetime
+    )
+# --- END OF CODE UPDATE ---
 
 st.set_page_config(page_title="Halbwertszeitrechner", page_icon=":material/home:")
 
